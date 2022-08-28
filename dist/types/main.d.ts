0e79042ee8841983e9c20f3a7e224457ba298507
Peter <stovka.peter@gmail.com>
2022-08-28 17:00:12 +0000

    wip

--- dist/types/main.d.ts
+++
@@ -1,5 +0,0 @@
-import Plugin from "./plugin";
-import type { PluginOptions } from "./plugin";
-import ViewComponentManager from "./view-component-manager";
-import Portal from './Components/Portal.vue';
-export { Plugin, PluginOptions, ViewComponentManager, Portal };
--- dist/types/plugin.d.ts
+++
@@ -1,8 +0,0 @@
-import type { App } from "vue";
-export interface PluginOptions {
-    components?: Record<string, Record<string, Record<string, Promise<any> | (() => Promise<any>)>>>;
-}
-declare const _default: {
-    install(app: App, options?: PluginOptions): any;
-};
-export default _default;
